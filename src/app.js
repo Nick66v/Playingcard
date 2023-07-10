4034171f72462941ef1373fac550e63e3083e8ee
Nick66v <136284543+Nick66v@users.noreply.github.com>
2023-07-10 17:17:05 +0000

    added colors

--- src/app.js
+++ src/app.js
@@ -6,10 +6,17 @@ return Math.floor(Math.random() * array.length)
     }
     let suit = suits[randomizer(suits)]
   
-    }
+    if (suit === "♦" || suit === "♥") {
+        topsuite.style.color = "red";
+      }
+    
     topsuite.innerHTML = suit
     const bottomsuite = document.querySelector("#bottomsuite")
-    bottomsuite.innerHTML = suit
+
+    if (suit === "♦" || suit === "♥") {
+        bottomsuite.style.color = "red";
+      }
+    bottomsuite.innerHTML = suit;
 
     const numbers = document.querySelector("#numbers")
     const cards = ["2","3","4","5","6","7","8","9","10","J","Q","K","A"]
